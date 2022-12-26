a5ddbb35f53d0a41618e3c60dd44cb4ed7958065
narutiga <100295602+narutiga@users.noreply.github.com>
2022-12-26 13:11:48 +0900

    🚧 Add footer componet

---
+++ src/component/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+
+/** @package */
+export const Footer: FC = () => {
+  return (
+    <div className="flex items-center justify-center h-16 bg-sub3">
+      <p className="text-white text-sm">@kino 2022</p>
+    </div>
+  );
+};
