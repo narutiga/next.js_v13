d21e182d82fce3ff3e549a56e1921255a35ded94
narutiga <100295602+narutiga@users.noreply.github.com>
2022-12-26 13:11:25 +0900

    🚧 Add header component

---
+++ src/component/Header/Header.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import Link from "next/link";
+import { MainNavigation } from "src/component/MainNavigation/MainNavigation";
+
+/** @package */
+export const Header: FC = () => {
+  return (
+    <div className="flex flex-col p-4 h-32 bg-main">
+      <Link href="/">
+        <p className="text-white text-3xl font-semibold">Next.js v13</p>
+      </Link>
+      <MainNavigation />
+    </div>
+  );
+};
