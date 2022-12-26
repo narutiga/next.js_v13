1c189988138a0d600a0d66193771ec0ac692a424
narutiga <100295602+narutiga@users.noreply.github.com>
2022-12-26 13:10:58 +0900

    🚧 Add title component

---
+++ src/component/PageTitle/PageTitle.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+
+/** @package */
+type Props = {
+  title: string;
+};
+
+export const PageTitle: FC<Props> = (props) => {
+  return <h1 className="m-8 text-sub2 text-xl font-medium">{props.title}</h1>;
+};
