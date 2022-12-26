4e6e2818680e2d9ff5bfbbe71dd5049ef6674f9b
narutiga <100295602+narutiga@users.noreply.github.com>
2022-12-26 12:33:25 +0900

    💄 Adding additional colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,14 @@ module.exports = {
     "src/component/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#ade2d1",
+        sub1: "#e6f7f1",
+        sub2: "#d1ade2",
+        sub3: "#e2d1ad",
+      },
+    },
   },
   plugins: [],
 };
