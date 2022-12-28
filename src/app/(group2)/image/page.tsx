f7a51d42aa2b141f33d2042f19746102e208ac61
narutiga <100295602+narutiga@users.noreply.github.com>
2022-12-28 12:27:39 +0900

    ✨ Add pictures

--- src/app/(group2)/image/page.tsx
+++ src/app/(group2)/image/page.tsx
@@ -1,9 +1,23 @@
 import { FC } from "react";
+import Image from "next/image";
+import narugaPic from "public/naruga.jpg";
+import tigaPic from "public/tiga.jpg";
 
 const Page: FC = () => {
   return (
-    <div>
-      <p>Image</p>
+    <div className="flex w-100 p-8">
+      <div className="relative w-[45%] pt-[45%]">
+        <Image
+          src={narugaPic}
+          alt="Picture of naruga"
+          fill
+          priority
+          sizes="50vw"
+        />
+      </div>
+      <div className="relative w-[45%] pt-[45%] ml-8">
+        <Image src={tigaPic} alt="Picture of tiga" fill priority sizes="50vw" />
+      </div>
     </div>
   );
 };
